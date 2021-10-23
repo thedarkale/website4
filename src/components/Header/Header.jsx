abae660a684f16bdc5929edffad6d3154c724e0b
bitakshat <akshatpal1232@gmail.com>
2021-10-23 10:05:02 +0530

    commented out code for quote component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import Quote from '../Quotes/Quote'
+// import Quote from '../Quotes/Quote'
 
 const Header = ({ title }) => (
   <header className="border-bottom border-color-light-grey">
     <div className="container">
       <h1 className="margin-none padding-top-2 padding-bottom-2">
-        <h2><u>Quote of the Day</u></h2>
-        <Quote />
+        <h1>{title}</h1>
+        {/* <h2><u>Quote of the Day</u></h2>
+        <Quote /> */}
       </h1>
     </div>
   </header>
-)
+);
 
 export default Header;
